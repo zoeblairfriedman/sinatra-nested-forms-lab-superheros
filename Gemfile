de32adb956d8b3b5e732dca2fd0ab5d83bc69135
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-02-24 10:07:03 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem 'thin', '1.6.4'
 gem 'shotgun', '0.9'
 gem 'pry', '0.10.3'
 gem 'require_all', '1.3.2'
+gem "activerecord", "~> 6.0.0", :require => 'active_record'
+gem "sinatra-activerecord"
 
 group :test do
   gem 'rspec', '3.3.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get "/" do
+        erb :super_hero
+    end
+
+    post "/team" do
+        binding.pry
+        @team = params
+        erb :team
+    end
 
 end
